8f5fca4a7c17cfabf93dc3a5631c4f58581eb2a2
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-27 15:34:21 +0200

    [setting] created some scss files to begin styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,9 +6,18 @@ export default {
 </script>
 
 <template>
-  <div>Header</div>
+  <header>
+    <div class="top-bar">
+      <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+  </header>
 </template>
 
-<style>
+<style lang="scss" scoped>
+  @use '../scss/general' as *;
+  @use '../scss/utils' as *;
+  @use '../scss/vars' as *;
+  @use '../scss/mixin' as *;
+
 
 </style>
