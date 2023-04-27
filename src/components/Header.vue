8d7f8854df48fbf47235f46cc779d2abc689f99f
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-27 16:06:28 +0200

    [add] header finished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,8 +7,22 @@ export default {
 
 <template>
   <header>
-    <div class="top-bar">
+    <div class="top-bar container">
       <img src="../assets/img/dc-logo.png" alt="">
+      <nav>
+        <ul>
+          <li><a href="#">characters</a></li>
+          <li><a href="#" class="active">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+          <li><a href="#">collectibles</a></li>
+          <li><a href="#">videos</a></li>
+          <li><a href="#">fans</a></li>
+          <li><a href="#">news</a></li>
+          <li><a href="#">shop</a></li>
+        </ul>
+      </nav>
     </div>
   </header>
 </template>
@@ -19,5 +33,32 @@ export default {
   @use '../scss/vars' as *;
   @use '../scss/mixin' as *;
 
-
+  .top-bar{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 81px;
+      img{
+        height: 55px;
+        margin: 10px 0 0 10px;
+      }
+      li{
+        display: inline-block;
+        height: 100%;
+        font-size: 10px;
+        margin: 0 10px;
+        
+        
+        a{
+          height: 100%;
+          color: $color-black;
+          border-bottom: 4px solid transparent;
+          padding: 29px 0;
+          &.active{
+            border-bottom-color: $color-blue;
+            color: $color-blue;
+          }
+        }
+      }
+  }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,9 +6,22 @@ export default {
 </script>
 
 <template>
-  main
+  <section>
+    main
+  </section>
 </template>
 
-<style>
+<style lang="scss" scoped>
+  @use '../scss/general' as *;
+  @use '../scss/utils' as *;
+  @use '../scss/vars' as *;
+  @use '../scss/mixin' as *;
 
+  section{
+    min-height: 300px; 
+    color: white;
+    // debug
+
+    background-color: $color-black;
+  }
 </style>
