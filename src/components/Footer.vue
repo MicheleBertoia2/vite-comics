c8554a2b9d262c0ef8becff9cf0882161edaa809
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-27 17:46:14 +0200

    [add] menu list on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,10 +9,55 @@ export default {
   <footer>
 
     <div class="f-top container">
-      <div class="f-left"></div>
+
+      <div class="f-left">
+
+        <ul>
+          <li><a href="#">dc comics</a></li>
+          <li><a href="#">dc comics</a></li>
+          <li><a href="#">dc comics</a></li>
+          <li><a href="#">dc comics</a></li>
+          <li><a href="#">dc comics</a></li>
+          <li><a href="#">dc comics</a></li>
+          <li><a href="#">dc comics</a></li>
+          <li><a href="#">dc comics</a></li>          
+        </ul>
+        
+        <ul>
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>                    
+        </ul>
+        
+        <ul>
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+        </ul>
+        
+        <ul>
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+          <li><a href="#">dc comics</a></li>          
+        </ul>
+
+      </div>
+
       <div class="f-right">
         <img src="../assets/img/dc-logo-bg.png" alt="">
       </div>
+
     </div>
 
     <div class="f-social container"></div>
@@ -27,14 +72,38 @@ export default {
   @use '../scss/mixin' as *;
 
   footer{
-    min-height: 300px;
+    height: 280px;
     background-image: url('../assets/img/footer-bg.jpg');
+    overflow: hidden;
     .f-top{
+      height: 100%;
       display: flex;
       justify-content: space-between;
-        .f-left{
-        border: 1px solid white;
+      .f-left{
+        height: 100%;
+        padding: 20px 0;
         width: 35%;
+        display: flex;
+        flex-flow: column wrap;
+        ul{
+          margin-bottom: 10px;
+          li{
+          &:first-child{
+             a{
+               color: $color-white;
+               text-transform: uppercase;      
+               font-weight: 500;
+               font-size: 14px;      
+             }
+           }
+           a {
+             color: lighten($color-darkgray, 10%);
+             font-size: 9px;           
+             }
+          }
+          
+        }
+        
       }
       .f-right{
         width: 40%;
