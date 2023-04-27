fd645f212853677b0ebd9f66921b898705c359d5
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-27 17:18:27 +0200

    [working] on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,9 +6,45 @@ export default {
 </script>
 
 <template>
-  <footer>Footer</footer>
+  <footer>
+
+    <div class="f-top container">
+      <div class="f-left"></div>
+      <div class="f-right">
+        <img src="../assets/img/dc-logo-bg.png" alt="">
+      </div>
+    </div>
+
+    <div class="f-social container"></div>
+
+  </footer>
 </template>
 
-<style>
+<style lang="scss" scoped>
+  @use '../scss/general' as *;
+  @use '../scss/utils' as *;
+  @use '../scss/vars' as *;
+  @use '../scss/mixin' as *;
+
+  footer{
+    min-height: 300px;
+    background-image: url('../assets/img/footer-bg.jpg');
+    .f-top{
+      display: flex;
+      justify-content: space-between;
+        .f-left{
+        border: 1px solid white;
+        width: 35%;
+      }
+      .f-right{
+        width: 40%;
+        img{
+          transform: translateY(-10%);
+        }
+      }
+    }
+    
+  }
+
 
 </style>
