18f3cdaf0d5e9ec92e97546fc9bb04187a0bde5f
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-28 15:30:09 +0200

    [add] static comic cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -22,12 +22,47 @@ export default {
 
         <div class="cards-container">
 
+          <div class="card">
+            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="American Vampire 1976">
+            <h4>american vampire 1976</h4>
+          </div>
+
+          <div class="card">
+            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="American Vampire 1976">
+            <h4>american vampire 1976</h4>
+          </div>
+
+          <div class="card">
+            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="American Vampire 1976">
+            <h4>american vampire 1976</h4>
+          </div>
+
+          <div class="card">
+            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="American Vampire 1976">
+            <h4>american vampire 1976</h4>
+          </div>
+
+          <div class="card">
+            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="American Vampire 1976">
+            <h4>american vampire 1976</h4>
+          </div>
+
+          <div class="card">
+            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="American Vampire 1976">
+            <h4>american vampire 1976</h4>
+          </div>
+
+          <div class="card">
+            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="American Vampire 1976">
+            <h4>american vampire 1976</h4>
+          </div>
+
         </div>
 
       </div>
 
       <div class="load-more">
-        <button></button>
+        <button>LOAD MORE</button>
       </div>
 
     </div>
@@ -47,8 +82,14 @@ export default {
       overflow: hidden;
     }
 
+    .container{
+      position: relative;
+      top: -25px;
+    }
+
     .content{
       background-color: $color-black;
+
       .label{
         height: 50px;
         width: 180px;
@@ -60,6 +101,40 @@ export default {
           line-height: 50px;
           font-size: 16px;
           font-weight: 700;
+        }
+      }
+
+      .cards-container{        
+        display: flex;
+        flex-wrap: wrap;
+        padding: 10px 0;
+        .card{
+          width: calc(100% / 6);
+          padding: 10px;
+          img{
+            width: 100%;
+            aspect-ratio: 1/1 ;
+
+          }
+          h4{
+            color: white;
+            text-transform: uppercase;
+            font-size: 10px;
+            padding: 10px
+            
+          }
+        }
+      }
+
+      .load-more{
+        @include centerFlex;
+        padding-bottom: 20px;
+        button{
+          border: none;
+          color: $color-white;
+          background-color: $color-blue;
+          font-weight: 700;
+          padding: 10px 50px;
         }
       }
     }
