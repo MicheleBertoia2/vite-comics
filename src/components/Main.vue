4d54e4c473951fe6488ddd470099f33be8660543
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-28 14:56:15 +0200

    [add] jumbo + working on main content

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,7 +7,31 @@ export default {
 
 <template>
   <section>
-    Content Goes here
+
+    <div class="jumbo">
+      <img src="../assets/img/jumbotron.jpg" alt="dc heroes">
+    </div>
+
+    <div class="content">
+
+      <div class="container">
+
+        <div class="label">
+          <span>current series</span>
+        </div>
+
+        <div class="cards-container">
+
+        </div>
+
+      </div>
+
+      <div class="load-more">
+        <button></button>
+      </div>
+
+    </div>
+
   </section>
 </template>
 
@@ -18,10 +42,26 @@ export default {
   @use '../scss/mixin' as *;
 
   section{
-    min-height: 100px; 
-    color: white;
-    // debug
+    .jumbo{
+      height: 300px;
+      overflow: hidden;
+    }
 
-    background-color: $color-black;
+    .content{
+      background-color: $color-black;
+      .label{
+        height: 50px;
+        width: 180px;
+        background-color: $color-blue;
+        text-align: center;
+        span{
+          text-transform: uppercase;
+          color: white;
+          line-height: 50px;
+          font-size: 16px;
+          font-weight: 700;
+        }
+      }
+    }
   }
 </style>
